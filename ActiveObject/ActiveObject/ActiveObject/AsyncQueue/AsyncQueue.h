3e8fcb54902e4c89f6a8ebd4f78dc0e519044c38
majiakun111 <majiakun111@sina.cn>
2019-04-21 18:00:43 +0800

    Update AsyncQueue.h

--- ActiveObject/ActiveObject/ActiveObject/AsyncQueue/AsyncQueue.h
+++ ActiveObject/ActiveObject/ActiveObject/AsyncQueue/AsyncQueue.h
@@ -10,16 +10,23 @@
 
 @class Database;
 
+typedef NS_OPTIONS(NSInteger, SqlType) {    
+    SqlForDDLType = 1 << 0, //数据库定义的sql
+    SqlForDMLType = 1 << 1, //数据库操作的sql
+    SqlForDQLType = 1 << 2, //数据库查询的sql
+};
+
+
 @interface AsyncQueue : NSObject
 
 + (instancetype)sharedInstance;
 
-- (void)inDatabase:(void (^)())block;
+- (void)inDatabase:(void (^)(void))block forSqlType:(SqlType)sqlType;
 
-- (void)inDeferredTransaction:(void (^)(BOOL *rollback))block;
+- (void)inDeferredTransaction:(void (^)(BOOL *rollback))block forSqlType:(SqlType)sqlType;
 
-- (void)inImmediateTransaction:(void (^)(BOOL *rollback))block;
+- (void)inImmediateTransaction:(void (^)(BOOL *rollback))block forSqlType:(SqlType)sqlType;
 
-- (void)inExclusiveTransaction:(void (^)(BOOL *rollback))block;
+- (void)inExclusiveTransaction:(void (^)(BOOL *rollback))block forSqlType:(SqlType)sqlType;
 
 @end
